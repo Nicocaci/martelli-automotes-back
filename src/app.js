e855b9b88aed9aa98c3014194e40987ebd975faf
Nicocaci <nicko.caci@gmail.com>
2025-05-28 10:15:47 -0300

    push

--- src/app.js
+++ src/app.js
@@ -17,8 +17,8 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: //"https://www.autosmartapp.com",
-        "http://localhost:5173",
+        origin: "https://www.autosmartapp.com",
+        //"http://localhost:5173",
         credentials: true,
     }
 });
