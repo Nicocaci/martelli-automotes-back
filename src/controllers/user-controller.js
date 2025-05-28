7c99fe67d759ce40be58ac28b50f3a42c8b48213
Nicocaci <nicko.caci@gmail.com>
2025-05-28 10:20:21 -0300

    push

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -154,7 +154,7 @@ class UsuarioController {
         sameSite: "None",
         maxAge: 24 * 60 * 60 * 1000,
         path: '/',
-        domain: "https://www.autosmartapp.com"
+        domain: ".autosmartapp.com"
         
       });
 
